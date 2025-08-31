d0c48ec5386facfb89ff5da5141b12ffef24dd64
hazratali-uydevelopers <hazratali.uydevelopers@gmail.com>
2025-08-31 17:06:52 +0600

    Connect to DB and erverless get/post

---
+++ netlify/functions/common/mongo.go
@@ -0,0 +1,53 @@
+package common
+
+import (
+    "context"
+    "os"
+    "sync"
+    "time"
+
+    "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var (
+    mongoClient *mongo.Client
+    mu          sync.Mutex
+)
+
+func GetMongoClient(ctx context.Context) (*mongo.Client, error) {
+    mu.Lock()
+    defer mu.Unlock()
+
+    if mongoClient != nil {
+        return mongoClient, nil
+    }
+
+    uri := os.Getenv("MONGODB_URI")
+    clientOpts := options.Client().ApplyURI(uri)
+    ctx2, cancel := context.WithTimeout(ctx, 10*time.Second)
+    defer cancel()
+
+    c, err := mongo.Connect(ctx2, clientOpts)
+    if err != nil {
+        return nil, err
+    }
+    if err := c.Ping(ctx2, nil); err != nil {
+        _ = c.Disconnect(ctx2)
+        return nil, err
+    }
+
+    mongoClient = c
+    return mongoClient, nil
+}
+
+func DisconnectClient(ctx context.Context) error {
+    mu.Lock()
+    defer mu.Unlock()
+    if mongoClient == nil {
+        return nil
+    }
+    err := mongoClient.Disconnect(ctx)
+    mongoClient = nil
+    return err
+}
