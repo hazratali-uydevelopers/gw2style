b147c2d49f5a7828567cf03c87aed14c051b701f
hazratali-uydevelopers <hazratali.uydevelopers@gmail.com>
2025-09-01 10:54:08 +0600

    Update connection timeout from 10sec to 30sec

--- netlify/functions/common/mongo.go
+++ netlify/functions/common/mongo.go
@@ -3,7 +3,6 @@ package common
 import (
 	"context"
 	"fmt"
-	"log"
 	"os"
 	"sync"
 	"time"
@@ -16,32 +15,6 @@ var (
 	mongoClient *mongo.Client
 	mu          sync.Mutex
 )
-
-// func GetMongoClient(ctx context.Context) (*mongo.Client, error) {
-//     mu.Lock()
-//     defer mu.Unlock()
-
-//     if mongoClient != nil {
-//         return mongoClient, nil
-//     }
-
-//     uri := os.Getenv("MONGODB_URI")
-//     clientOpts := options.Client().ApplyURI(uri)
-//     ctx2, cancel := context.WithTimeout(ctx, 10*time.Second)
-//     defer cancel()
-
-//     c, err := mongo.Connect(ctx2, clientOpts)
-//     if err != nil {
-//         return nil, err
-//     }
-//     if err := c.Ping(ctx2, nil); err != nil {
-//         _ = c.Disconnect(ctx2)
-//         return nil, err
-//     }
-
-//     mongoClient = c
-//     return mongoClient, nil
-// }
 
 func GetMongoClient(ctx context.Context) (*mongo.Client, error) {
 	mu.Lock()
@@ -56,10 +29,8 @@ func GetMongoClient(ctx context.Context) (*mongo.Client, error) {
 		return nil, fmt.Errorf("MONGODB_URI is empty")
 	}
 
-	log.Println("Connecting to MongoDB URI starts with:", uri[:10])
-
 	clientOpts := options.Client().ApplyURI(uri)
-	ctx2, cancel := context.WithTimeout(ctx, 10*time.Second)
+	ctx2, cancel := context.WithTimeout(ctx, 30*time.Second)
 	defer cancel()
 
 	c, err := mongo.Connect(ctx2, clientOpts)
